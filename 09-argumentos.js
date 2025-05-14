2245e9e2adcfe61acb07ceabe6da256f0be02791
marcestruch <marcstruchmalonda@gmail.com>
2025-05-14 12:23:08 +0200

    Anyadido nuevo archivo 09-argumentos

---
+++ 09-argumentos.js
@@ -0,0 +1,15 @@
+function suma(a){
+  return a+2;
+}
+
+let resultado = suma(5);
+console.log(resultado); // 7
+// En este caso, la función suma toma un argumento a y le suma 2.
+
+function suma1(a, b){
+  return a+b;
+}
+
+let resultado = suma(5, 3);
+console.log(resultado); // 8
+// En este caso, la función suma1 toma dos argumentos a y b y devuelve su suma.
