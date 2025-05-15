e002d8a1a3d47cef9f9a3740407b704180ed9334
marcestruch <marcstruchmalonda@gmail.com>
2025-05-15 14:15:16 +0200

    falsy implementado

---
+++ 14-falsy.js
@@ -0,0 +1,14 @@
+// short-circuit
+
+// falsy
+// Falsy values in JavaScript are values that evaluate to false when used in a boolean context.
+
+// Falso
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+
+let nombre = "";
